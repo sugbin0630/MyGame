46b1e27bb111f28c242289764373d534b6df8bb6
sugbin0630 <sugbin0630@gmail.com>
2023-10-15 06:43:49 -0400

    commit

--- src/Game_Runner.java
+++ src/Game_Runner.java
@@ -2,5 +2,20 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Game_Runner extends JFrame{
-    
+    private Container c = getContentPane();
+    final int WIDTH = 600;
+    final int HEIGHT = 600;
+
+    Game_Runner() {
+        c.setLayout(null);
+        setTitle("Game");
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(true);
+        c.setBackground(new Color(0, 0, 0));
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        
+    }
 }
